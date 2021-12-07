ae69e1060bdcfd0303ff17ca317f1639c1186dcb
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-12-07 18:39:43 +0200

    html/styling

--- quiz_app/src/views/Quiz.vue
+++ quiz_app/src/views/Quiz.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="quiz">
-    <h1>Quiz</h1>
-<!--    <QuizComponent/>-->
+  <div class="container-app">
+
+    <div class="container-quiz">
+      <div class="header-quiz">
+        <h1>Quiz App</h1>
+      </div>
+        <div class="box-question">
+          <h2>Question</h2>
+        </div>
+        <div class="box-propositions">
+          <ul>
+            <li>Test</li>
+          </ul>
+        </div>
+      <div class="box-score">
+        <h1>Your score:</h1>
+        <div class="btn-restart">
+        </div>
+      </div>
+      <div class="footer-quiz">
+      </div>
+    </div>
   </div>
 </template>
 
