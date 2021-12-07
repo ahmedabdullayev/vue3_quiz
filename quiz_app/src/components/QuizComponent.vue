0deff678496e6020c12bfa349ab3eacfd8fa05bd
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-12-07 18:11:10 +0200

    structuring a bit

---
+++ quiz_app/src/components/QuizComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="QuizComponent">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'QuizComponent',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ quiz_app/src/views/Quiz.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="quiz">
+    <h1>Quiz</h1>
+<!--    <QuizComponent/>-->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import QuizComponent from "@/components/QuizComponent.vue";
+export default defineComponent({
+  name: 'Quiz',
+  components: {
+    // QuizComponent,
+  },
+});
+</script>
